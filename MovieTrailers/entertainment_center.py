2900e230bf863c3a3d2ed118b278f91b4c968b0c
Charlie Ngo <charlie.ngo@gmail.com>
2017-04-08 13:19:05 -0400

    Entertainment Center

---
+++ MovieTrailers/entertainment_center.py
@@ -0,0 +1,13 @@
+import media
+
+doctor_strange = media.Movie("Doctor Strange"
+                             ,"The Origin of Doctor Steven Strange the Sorcerer Supreme"
+                             ,"http://www.denofgeek.com/us/pictures/doctor-strange-movie#0"
+                             ,"https://www.youtube.com/watch?v=HSzx-zryEgM"
+                             )
+guardians_of_the_galaxy = media.Movie("Guardians of the Galaxy"
+                                     ,"A group of misfits save the galaxy."
+                                     ,"http://www.film-rezensionen.de/wp-content/uploads/2014/08/Guardians-of-the-Galaxy.jpg"
+                                     ,"https://www.youtube.com/watch?v=d96cjJhvlMA"
+                                     )
+print(guardians_of_the_galaxy.storyline)
